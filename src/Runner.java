330addf62a64a36b38c38ede302c0a8f866321f3
ProgramStorage <34112036+ProgramStorage@users.noreply.github.com>
2017-11-29 19:03:24 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.util.Random;
+
 public class Runner {
 
 	private static String[] firstName = {"First", "John", "Jane", "Nathan", "Joe", "Steve", "Liam", "Noah", "Jake", "Logan", "David"};
@@ -17,9 +20,41 @@ public class Runner {
 	
 	}
 	
-	public static String randomStudent() {
-		return Person.getFirstName() + Person.getFamilyName() + Student.getGPA();
-		}
+	public static Student randomStudent() {
+		int ACT = (int)(Math.random())*36+1;
+		int APClasses = (int)(Math.random()*11);
+		int IQ = (int)(Math.random() * 200 + 1);
+		Random r2 = new Random();
+		//highest gpa is 100 x 1.1 which is 110
+		double GPA = (r2.nextInt(1210)) / 10;
+		
 
+		Random r = new Random();
+		int red = r.nextInt(256);
+		int green = r.nextInt(256);
+		int blue = r.nextInt(256);
+
+		Color eyeColor = new Color(red,green,blue);
+		
+		Random r1 = new Random();
+		return new Student(firstName[r1.nextInt(firstName.length)],familyName[r1.nextInt(familyName.length)], eyeColor , IQ, GPA, ACT, APClasses);
+		
+		}
+	
+	public static Teacher randomTeacher()
+	{
+		int IQ = (int)(Math.random() * 200 + 1);
+		
+		Random r2 = new Random();
+		int red = r2.nextInt(256);
+		int green = r2.nextInt(256);
+		int blue = r2.nextInt(256);
+
+		Color eyeColor = new Color(red,green,blue);
+		
+		Random r = new Random();
+		return new Teacher (firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], eyeColor, IQ, title[r.nextInt(title.length)], 
+				subject[r.nextInt(subject.length)]);
 	}
 
+	}
