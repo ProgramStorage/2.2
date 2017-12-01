03934c83200897710af53a194070945699fd12d4
ProgramStorage <34112036+ProgramStorage@users.noreply.github.com>
2017-11-30 22:06:24 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,5 @@
-import java.awt.Color;
 import java.util.Random;
+import java.lang.Math; 
 
 public class Runner {
 
@@ -8,53 +8,36 @@ public class Runner {
 	private static String[] subject = {"AP Computer Science", "Chemistry", "AP Calculus", "Macro Economics", "Physical Education", "AP United States History", "Statistics", "Algebra", "Geometry", "Global Studies"};
 	private static String[] title = {"Mr", "Mrs", "Ms", "Miss", "Dr", "Professor"};
 	
-	public static void main(String[] args) {
-	
-		new Teacher(null, null, null, 0, null, null);
-		Person Student = new Student(null, null, null, 0, 0, 0, 0);
-		Classroom Classroom = new Classroom(null, null, null, 0, null, Student);
+	public static void main(String[] args) 
+	{
 		
-		System.out.print (Classroom.printClass() + Classroom.classAverage());
-		
-		randomStudent();
-	
+		 
+		System.out.println(randomTeacher());
+		System.out.println(randomStudent());
 	}
+	
 	
 	public static Student randomStudent() {
 		int ACT = (int)(Math.random())*36+1;
 		int APClasses = (int)(Math.random()*11);
-		int IQ = (int)(Math.random() * 200 + 1);
-		Random r2 = new Random();
-		//highest gpa is 100 x 1.1 which is 110
-		double GPA = (r2.nextInt(1210)) / 10;
+		int IQ = (int)(Math.random()) * 200 + 1;
+		int shoeSize = (int)(Math.random() * 14 + 1);
+		double GPA = (int)(Math.random() * 110 + 1);
 		
-
+		
 		Random r = new Random();
-		int red = r.nextInt(256);
-		int green = r.nextInt(256);
-		int blue = r.nextInt(256);
-
-		Color eyeColor = new Color(red,green,blue);
-		
-		Random r1 = new Random();
-		return new Student(firstName[r1.nextInt(firstName.length)],familyName[r1.nextInt(familyName.length)], eyeColor , IQ, GPA, ACT, APClasses);
+		return new Student(firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], shoeSize , IQ, GPA, ACT, APClasses);
 		
 		}
 	
 	public static Teacher randomTeacher()
 	{
-		int IQ = (int)(Math.random() * 200 + 1);
-		
-		Random r2 = new Random();
-		int red = r2.nextInt(256);
-		int green = r2.nextInt(256);
-		int blue = r2.nextInt(256);
-
-		Color eyeColor = new Color(red,green,blue);
+		int IQ = (int)(Math.random()) * 200 + 1;
+		int shoeSize = (int)(Math.random() * 14 + 1);
 		
 		Random r = new Random();
-		return new Teacher (firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], eyeColor, IQ, title[r.nextInt(title.length)], 
+		return new Teacher(firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], shoeSize, IQ, title[r.nextInt(title.length)], 
 				subject[r.nextInt(subject.length)]);
 	}
 
-	}
+}
